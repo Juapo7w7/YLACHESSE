6b238db34fbe1f6cac45adf0166b0e59cd92b09c
Oliver MOlina <Olivermolina697@gmail.com>
2023-06-13 10:28:42 -0600

    Porfin se hizo el examen papi

---
+++ Soporte.py
@@ -0,0 +1,4 @@
+print ("if por algunos problemas los cuales no se como poder hacerlos ")
+print ("tablas y sus variantes, por el hecho de que se me olvida el cuerpo de como se hace ")
+print ("la normalizacion ")
+print ("El pseudocodigo")
